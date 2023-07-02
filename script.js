aa56e5b9c6d9ea21dde0dafbacdbfded0a4bd903
pathik455 <82212939+pathik455@users.noreply.github.com>
2023-07-02 16:51:03 +0530

    clock ready

---
+++ script.js
@@ -0,0 +1,20 @@
+const timer = document.getElementById("time");
+const button = document.getElementById("stop-timer");
+const button2 = document.getElementById("start-timer");
+
+function showTime(){
+    const currentTime = new Date();
+    const time = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()} :: ${currentTime.getMilliseconds()}`;
+    timer.innerText = time;
+}
+
+let interval = setInterval(showTime, 100);
+
+button.addEventListener("click", () =>{
+    let insertedTime = document.getElementById("put-time").value;
+    setTimeout(function( ) { clearInterval( interval ); }, insertedTime*1000);
+})
+
+button2.addEventListener("click", () =>{
+    interval = setInterval(showTime, 100);
+})
